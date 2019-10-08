bd37522e591a38ce18ca86e8414ac393d48f8f56
TuanAnh <youremail@domain.com>
2019-10-08 08:49:03 +0700

    add new branch

--- src/index.js
+++ src/index.js
@@ -1,65 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import { ENGINE_METHOD_DSA } from 'constants';
-class Note extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            Sum: 0,
 
-        };
-    }
-       
-    showIncrease = () => {
-        this.setState({
-            Sum : this.state.Sum +1,
-        });
-        
-       
-    }
-    showDecrease = () => {
-        this.state.Sum = this.state.Sum-1;
-        this.setState(this.state);
-    }
-	render() {
-		return (
-			<div className="Container">
-                <div className="img-left">
-                    <img  src={this.props.src} />
-                </div>
-                <div className="content-right">
-                    <h2>{this.props.children}</h2>
-                    <span>Giá: {this.props.gia} VND</span>
-                    <div>
-                        {
-                            this.state.Sum > 0 ?(
-                                 /* ham buttonGiam truyen props gia nguoc lai List */ 
-                            <button onClick={()=>{this.showDecrease(); this.props.buttonGiam(this.props.gia)}}> - </button>
-                            ):""
-                        }
-                        {this.state.Sum>0?<span>{this.state.Sum}</span>:""}
-                        <button onClick={(event) =>{this.props.onNhan(this.props.gia);this.showIncrease()}}> + </button>
-                        { /* ham onNhan truyen props gia nguoc lai List */ }
-                        
-                        
-                    </div>
-                    
-               
-                </div>
-                
-                
-				<div className="clear"></div>
-			</div>
-		);
-	}
-}
-
-class ShowBanner extends React.Component{
-    render(){
-        return(
+class ShowBanner extends React.Component {
+    render() {
+        return (
             <div className="Banner-footer">
                 <div className="Banner-Content">
                     <strong><span>Số lượng : {this.props.soluong}</span></strong>
@@ -69,84 +14,158 @@ class ShowBanner extends React.Component{
         );
     }
 }
-class Container extends React.Component{
-    constructor(props){
-		super(props);
-		this.state = {
-            isShow: false,
-            gia: 0,
-            soluong: 0
+
+class Note extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            sum: 0,
         };
     }
-    onShowGia=(e)=>{
-		this.setState({
-            isShow:true, 
-            gia: +this.state.gia+ +e,
-            soluong: +this.state.soluong+ +1,
-        });
-    }
-    GiamGia = (e)=>{
+
+    addFood=()=>{
         this.setState({
-            gia: parseInt(this.state.gia)-e,
-            soluong: parseInt(this.state.soluong)-1,
+            sum: ++this.state.sum,
         });
+        this.props.curentAdd(this.props.itemFood);
     }
 
-    render(){
-        return(
-            //onShowGia tang gia va tang so luong o banner
-            //GiamGia giam gia va giam so luong o banner
-            <div>
-                <List onNhanList={this.onShowGia} onNhanGiamList={this.GiamGia} />
-                {
-                    //khi bam nut them or so luong lon hon 0 thi show banner
-                    this.state.soluong > 0?(
-                        this.state.isShow?<ShowBanner soluong={this.state.soluong} tongtien={this.state.gia}/>:""
-                    ):""
-                }
-                
+    subFood = () => {
+        this.setState({
+            sum: --this.state.sum,
+        });
+        this.props.curentSub(this.props.itemFood);
+    }
+
+    render() {
+        const { itemFood } = this.props;
+        return (
+            <div className="Container">
+                <div className="img-left">
+                    <img src={itemFood.srcHinh} />
+                </div>
+                <div className="content-right">
+                    <h2>{itemFood.Name}</h2>
+                    <span>Giá: {itemFood.Gia} VND</span>
+                    <div>
+                        {
+                            this.state.sum > 0 ? (
+                                <button onClick={this.subFood}> - </button>
+                            ) : ""
+                        }
+                        {this.state.sum > 0 ? <span>{this.state.sum}</span> : ""}
+                        <button onClick={this.addFood}> + </button>
+                    </div>
+                </div>
+                <div className="clear"></div>
             </div>
         );
     }
 }
-class List extends React.Component{
-	constructor(props){
-		super(props);
-		this.state = {
-			mang: [
-				{srcHinh: "images/nemchua.jpeg", Name: "Nem Chua Rán", Gia: "5000"},
-				{srcHinh: "images/trasua.jpeg", Name: "Trà Sữa", Gia: "10000"},
-				{srcHinh: "images/tradao.jpeg", Name: "Trà Đào", Gia: "15000"}
-			]
-		};
+
+class List extends React.Component {
+    constructor(props) {
+        super(props);
     }
 
-    onNhanNote = (e) =>{ 
-        // tham so e chua props gia
-        this.props.onNhanList(e);
-    }
-    onNhanGiam = (e)=>{
-        //tham so e chua props gia
-        this.props.onNhanGiamList(e);
-        
+    addOrders = (itemFood) => {
+        this.props.addToOrders(itemFood);
     }
 
-	render(){
-		return(
-			<div>
-				{
-					this.state.mang.map((note,index) => {
-						return <Note key={index} src={note.srcHinh} buttonGiam={this.onNhanGiam}   onNhan={this.onNhanNote} gia={note.Gia}>{note.Name}</Note>
+    subOrders = (itemFood) => {
+        this.props.subToOrders(itemFood);
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    this.props.foods.map((note, index) => {
+                        return <Note key={index}
+                            itemFood={note}
+                            curentAdd={this.addOrders}
+                            curentSub={this.subOrders}
+                        >
+                        </Note>
                     })
                 }
-			</div>
-		);
-	}
+            </div>
+        );
+    }
 }
+
+class Container extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            foods: [
+                { srcHinh: "images/nemchua.jpeg", Name: "Nem Chua Rán", Gia: 5000 },
+                { srcHinh: "images/trasua.jpeg", Name: "Trà Sữa", Gia: 10000 },
+                { srcHinh: "images/tradao.jpeg", Name: "Trà Đào", Gia: 15000 }
+            ],
+            checkout: 0,
+            quantity: 0,
+            orders: []
+        };
+    }
+
+    changeOrder = (food, amount) =>{
+        const { orders } = this.state
+        const foodOrder = orders.find(item => item.Name === food.Name)
+        this.setState({
+            quantity: this.state.quantity + amount,
+            orders  : foodOrder ? orders.map(item => {
+                if (item.Name === food.Name) {
+                  let currentTotal = item.total + amount;
+                  return {
+                    ...item,
+                    total: currentTotal,
+                    price: currentTotal * food.Gia
+                  }
+                }
+                return item;
+                }) : [
+                    ...orders,
+                    {
+                        Name: food.Name,
+                        total: 1,
+                        price: food.Gia
+                    }
+                ],
+        }, ()=>{
+            var a =0;
+            this.state.orders.map(item=>{
+                a += item.price;
+                this.setState({
+                    checkout: a
+                });
+            })
+        })
+    }
+    
+    add = (food) => this.changeOrder(food,1);
+    sub = (food) => this.changeOrder(food,-1);
+
+    render() {
+        return (
+            <div>
+                <List foods={this.state.foods}
+                    addToOrders={this.add}
+                    subToOrders={this.sub} />
+                {
+                    this.state.quantity > 0 ? (
+                        <ShowBanner soluong={this.state.quantity} tongtien={this.state.checkout} />
+                    ) : ""
+                }
+            </div>
+        );
+    }
+}
+
 ReactDOM.render(
-	<div>
-		<Container />
-	</div>
-	, 
-	document.getElementById('root'));
-serviceWorker.unregister();
+    <div>
+        <Container />
+    </div>
+    ,
+    document.getElementById('root'));
